bbd4e62ec763c02abc8fb159dc2fc3cba37a8c2b
Eduard <claygod@yandex.ru>
2016-07-22 15:02:53 +0300

    Add files via upload

--- ph_api.go
+++ ph_api.go
@@ -9,6 +9,10 @@ import (
 	"runtime"
 )
 
+// PiHex Library generates a hexadecimal number sequence in the number Pi in
+// the range from 0 to 10,000,000. To calculate using "Bailey-Borwein-Plouffe"
+// algorithm, instructions that was published by David September 17, 2006.
+
 // New - create a new PiHex-struct
 func New() *Pi {
 	pi := &Pi{}
@@ -20,11 +24,11 @@ func New() *Pi {
 // Get - 'num' hexadecimal digits in the number Pi since 'start' position.
 // If the inputs exceed the permissible values, it returns an empty slice.
 //  Arguments:
-// 'start' - start number,
-// 'num' - how to calculate the numbers
+// start - start number
+// num - how to calculate the numbers
 //  Return:
 // slice bytes 0 to 15
-func (pi *Pi) Get (start int, num int) []byte {
+func (pi *Pi) Get(start int, num int) []byte {
 	var out []byte
 	if start <= CLIMIT &&
 		start+num < CLIMIT &&
