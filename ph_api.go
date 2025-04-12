57e37d946510eb9927cc38f1da8239a245fa48e9
Eduard <claygod@yandex.ru>
2025-04-12 06:31:22 +0300

    Update ph_api.go

--- ph_api.go
+++ ph_api.go
@@ -2,7 +2,7 @@ package PiHex
 
 // PiHex
 // API
-// Copyright © 2016-2024 Eduard Sesigin. All rights reserved. Contacts: <claygod@yandex.ru>
+// Copyright © 2016-2025 Eduard Sesigin. All rights reserved. Contacts: <claygod@yandex.ru>
 
 import (
 	"runtime"
